7efa8ac6b352bf925e8b3a56e6ab9d436b7f41bc
nathan-britten <me@nathanbritten.com>
2021-02-25 08:43:02 +0000

    production build

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+
 import { connect } from 'react-redux';
 import GoogleAuth from './GoogleAuth';
 import SearchBar from './SearchBar';
